484959552d713d0f3348751c4e0664a164113d49
GakutaroNishimura <nishi.g@quark.ap.titech.ac.jp>
2023-06-09 13:58:11 +0900

    RF_Vpp and amplitude

--- FFT2.py
+++ FFT2.py
@@ -5,44 +5,13 @@ import glob
 import os
 import time as tm
 
-#f_file = 223
-#l_file = 243
-#f_file = 244
-#l_file = 245
-f_file = 246
-l_file = 247
-f_file = 248
-l_file = 249
-f_file = 250
-l_file = 251
-f_file = 252
-l_file = 253
-f_file = 254
-l_file = 255
-f_file = 256
-l_file = 257
-f_file = 258
-l_file = 259
-f_file = 260
-l_file = 261
-f_file = 262
-l_file = 263
-#f_file = 264
-#l_file = 265
-#f_file = 266
-#l_file = 267
-#f_file = 268
-#l_file = 269
 f_file = 270
 l_file = 271
-f_file = 272
-l_file = 273
-#f_file = 274
-#l_file = 275
-#f_file = 276
-#l_file = 277
-f_file = 278
-l_file = 297
+V_pp = 90
+
+Srow = 10
+Sfooter = 20
+
 file_path = [] #データのpathをしまうlist.""で囲まれた文字列のリストになる.
 for i in range(f_file, l_file+1):
     path = glob.glob("./data/scope_%d.csv"% i)
@@ -55,13 +24,10 @@ os.makedirs(dir_path, exist_ok=True)
 
 freq_list = []
 
-frequency_spectrum1_mean = np.array([0.0 for i in range(982)])
-frequency_spectrum2_mean = np.array([0.0 for i in range(982)])
-
 # CSVファイルからデータを読み込む
 for i in range(int(len(file_path)/2)):
-    data1 = pd.read_csv(file_path[2*i], names=["time", "RFsignal", "PickUpsignal", "sync2"], skiprows=10, skipfooter=10, engine="python")
-    data2 = pd.read_csv(file_path[2*i+1], names=["time", "RFsignal", "PickUpsignal", "sync2"], skiprows=10, skipfooter=10, engine="python")    
+    data1 = pd.read_csv(file_path[2*i], names=["time", "RFsignal", "PickUpsignal", "sync2"], skiprows=Srow, skipfooter=Sfooter, engine="python")
+    data2 = pd.read_csv(file_path[2*i+1], names=["time", "RFsignal", "PickUpsignal", "sync2"], skiprows=Srow, skipfooter=Sfooter, engine="python")    
     # 時刻と電圧データの列を取得
     time1 = data1["time"].values
     voltage1 = data1["PickUpsignal"].values
@@ -75,12 +41,22 @@ for i in range(int(len(file_path)/2)):
     #time = data["time"].values[200:1900]
     #voltage = data["PickUpsignal"].values[200:1900]
     
-    #time = data["time"].values[1000:19900]
-    #voltage = data["PickUpsignal"].values[1000:19900]
+    #time1 = data1["time"].values[1000:19900]
+    #voltage1 = data1["PickUpsignal"].values[1000:19900]
 
-    #time = data["time"].values[2000:19900]
-    #voltage = data["PickUpsignal"].values[2000:19900]
+    #time2 = data2["time"].values[1000:19900]
+    #voltage2 = data2["PickUpsignal"].values[1000:19900]
+
+    time1 = data1["time"].values[2000:19900]
+    voltage1 = data1["PickUpsignal"].values[2000:19900]
+
+    time2 = data2["time"].values[2000:19900]
+    voltage2 = data2["PickUpsignal"].values[2000:19900]
     
+    Npoints = len(time1)
+    frequency_spectrum1_mean = np.array([0.0 for i in range(Npoints)])
+    frequency_spectrum2_mean = np.array([0.0 for i in range(Npoints)])
+
     # サンプリング周波数の計算
     sampling_rate = 1 / (time1[1] - time1[0])  # 時刻の間隔からサンプリング周波数を求める
 
@@ -100,33 +76,35 @@ for i in range(int(len(file_path)/2)):
     #print(matrix[:10])
     
 frequency_spectrum1_mean = frequency_spectrum1_mean/int(len(file_path)/2)
-frequency_spectrum2_mean = frequency_spectrum2_mean/int(len(file_path)/2)    
+frequency_spectrum2_mean = frequency_spectrum2_mean/int(len(file_path)/2)   
+
+print(int(len(file_path)/2))
 
 """
     # フーリエ変換結果のプロット
-    fig, ax = plt.subplots(nrows=2)
-    #ax[0].plot(frequencies, np.abs(frequency_spectrum), ".")
-    ax[0].plot(frequencies, np.abs(frequency_spectrum), ".", label="RF Vpp = " + str(150) + "[V]")
-    ax[0].set_xlim(15000, 25000)
-    ax[0].set_ylim(0, 5)
-    ax[0].grid()
-    ax[0].legend()
-    ax[1].plot(time, voltage, ".")
-    
-    plt.savefig(dir_path + str(i) + ".png")
-    #plt.show()
-    #tm.sleep(1000)
-    plt.close()
+fig, ax = plt.subplots(nrows=2)
+#ax[0].plot(frequencies, np.abs(frequency_spectrum), ".")
+ax[0].plot(frequencies1, np.abs(frequency_spectrum1), ".", label="RF Vpp = " + str(150) + "[V]")
+ax[0].set_xlim(17000, 19000)
+ax[0].set_ylim(0, 20)
+ax[0].grid()
+ax[0].legend()
+ax[1].plot(time1, voltage1, ".")
 
-    freq_list.append(np.max(np.abs(frequency_spectrum)))
-    
+#plt.savefig(dir_path + str(i) + ".png")
+plt.show()
+tm.sleep(1000)
+plt.close()
+
+freq_list.append(np.max(np.abs(frequency_spectrum1)))
+   
 plt.plot(np.linspace(1, len(file_path), len(file_path)), freq_list, ".")
 plt.grid()
 plt.xlabel("nubmer of measurement")
 plt.ylabel("amplitude")
 plt.savefig(dir_path + "amplitude.png")
 
-"""
+#"""
 
 """
 
@@ -144,13 +122,24 @@ plt.grid(True)
 plt.show()
 """
 
+min_freq = 17000
+max_freq = 19000
+
+indices = np.where((frequencies1 >= min_freq) & (frequencies1 <= max_freq))
+amplitude = frequency_spectrum1_mean[indices]
+max_amp = np.max(amplitude)
+
+
 fig, ax = plt.subplots(nrows=2)
-ax[0].plot(frequencies1, frequency_spectrum1_mean, ".")
-ax[0].set_xlim(13000, 25000)
-ax[0].set_ylim(0.0, 0.3)
+ax[0].plot(frequencies1, frequency_spectrum1_mean, ".", label="max amplitude = {:.1f}".format(max_amp) + "\n RF $V_{\mathrm{p-p}}$ = %d" %V_pp)
+ax[0].set_xlim(min_freq, max_freq)
+#ax[0].set_ylim(0.0, 20)
 ax[0].grid()
+ax[0].legend()
 ax[1].plot(frequencies2, frequency_spectrum2_mean, ".")
-ax[1].set_xlim(13000, 25000)
-ax[1].set_ylim(0.0, 0.3)
+ax[1].set_xlim(min_freq, max_freq)
+#ax[1].set_ylim(0.0, 10)
 ax[1].grid()
-plt.show()
+#plt.show()
+plt.savefig(dir_path + "amp_compare.pdf")
+
