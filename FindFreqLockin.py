942cdbc246b538da7c376fc020bdb9425304ae85
GakutaroNishimura <nishi.g@quark.ap.titech.ac.jp>
2023-06-26 19:10:03 +0900

    determined f0 and delta

--- FindFreqLockin.py
+++ FindFreqLockin.py
@@ -12,10 +12,12 @@ def FindFreqDeltaLockin(StartF, EndF, Naverage, Time, V_mean):
     FreqList = []
     deltaList = []
     LockinList = []
-    LockinValue = 0
+    
     deltaRange = np.linspace(0., 2*math.pi, 50)
-    for f0 in range(StartF, EndF, 200):
+    #deltaRange = np.linspace(0.7*math.pi, 1.3*math.pi, 10)
+    for f0 in range(StartF, EndF, 10):
         for delta in deltaRange:
+            LockinValue = 0
             for i in range(Naverage):
                 LockinValue += V_mean[i]*math.cos(2*math.pi*f0*Time[i]+delta)
                 if i == Naverage-1:
@@ -28,8 +30,9 @@ def FindFreqDeltaLockin(StartF, EndF, Naverage, Time, V_mean):
 def FindFreqLockin(StartF, EndF, Naverage, Time, V_mean):
     FreqList = []
     LockinList = []
-    LockinValue = 0
-    for f0 in range(StartF, EndF, 10):
+    
+    for f0 in range(StartF, EndF, 1):
+        LockinValue = 0
         for i in range(Naverage):
             LockinValue += V_mean[i]*math.cos(2*math.pi*f0*Time[i]+math.pi)
             #LockinValue += V_mean[i]*math.cos(2*math.pi*f0*Time[i])
@@ -38,6 +41,13 @@ def FindFreqLockin(StartF, EndF, Naverage, Time, V_mean):
                 LockinList.append(LockinValue)
                     
     return FreqList, LockinList
+
+def FinalLockin(f0, delta, Naverage, Time, V_mean):
+    LockinValue = 0
+    for i in range(Naverage):
+        LockinValue += V_mean[i]*math.cos(2*math.pi*f0*Time[i]+delta)
+        #LockinValue += V_mean[i]*math.cos(2*math.pi*f0*Time[i])
+    return LockinValue
 
 StartNo = FileInfo.GetMaxFileNumber() + 1
 StopNo  = conf.NumOfDataAcquisition
@@ -59,20 +69,22 @@ for i in range(len(data_list)):
         V_mean = np.array([0.0 for i in range(len(V))])
     V_mean += V
 
-FreqList, deltaList, LockinList = FindFreqDeltaLockin(17000, 21000, 1000, Time, V_mean)
+#FreqList, deltaList, LockinList = FindFreqDeltaLockin(17000, 19000, 1000, Time, V_mean)
 
-#FreqList, LockinList = FindFreqLockin(17000, 20000, 1000, Time, V_mean)
+#FreqList, LockinList = FindFreqLockin(17000, 19000, 1000, Time, V_mean)
 
-#"""
+LockinValue = FinalLockin(18270, 5.394654, 1000, Time, V_mean)
+
+"""
 fig = plt.figure()
 ax = fig.add_subplot(projection='3d')
 ax.scatter(FreqList, deltaList, LockinList)
 plt.show()
-#"""
-
 """
+
+#"""
 fig = plt.figure()
 ax = fig.add_subplot()
 ax.scatter(FreqList, LockinList)
 plt.show()
-"""
+#"""
